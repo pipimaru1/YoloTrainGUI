add0c07f8e8dfef7d41f4eec820c5b46cf94fbc5
YUUICHIROU SHISHIDO <yuuichirou_shishido@global.komatsu>
2025-08-25 13:53:11 +0900

    text

--- update.bat
+++ update.bat
@@ -1,3 +1,4 @@
 xcopy /y /d .\x64\Release\*.exe \\140.81.145.4\public\020DATA\yolo_tools
 xcopy /y /d .\YoloTrainGUI\*.ini \\140.81.145.4\public\020DATA\yolo_tools
+xcopy /y /d .\YoloTrainGUI\*.py \\140.81.145.4\public\020DATA\yolo_tools
 
